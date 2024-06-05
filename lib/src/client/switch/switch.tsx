3c0cbebd99b6454b7ad41a9331339e8f825252c1
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-06-05 23:09:38 +0530

    Handle theme switching

--- lib/src/client/switch/switch.tsx
+++ lib/src/client/switch/switch.tsx
@@ -1,8 +1,12 @@
-import { HTMLProps, ReactNode } from "react";
+import { HTMLProps, ReactNode, useCallback } from "react";
 import styles from "./switch.module.scss";
+import { modes, useStore } from "../../utils";
 
 export interface SwitchProps extends HTMLProps<HTMLDivElement> {
-  children?: ReactNode;
+  /** Diameter of the color switch */
+  size?: number;
+  /** Skip system colorScheme while toggling */
+  skipSystem?: boolean;
 }
 
 /**
@@ -15,11 +19,16 @@ export interface SwitchProps extends HTMLProps<HTMLDivElement> {
  *
  * @source - Source code
  */
-export const Switch = ({ children, ...props }: SwitchProps) => {
+export const Switch = ({ size, skipSystem, ...props }: SwitchProps) => {
+  const [state, setState] = useStore();
+  const handleModeSwitch = useCallback(() => {
+    let index = modes.indexOf(state.mode);
+    if (skipSystem && index === modes.length - 1) index = 0;
+    setState({
+      ...state,
+      mode: modes[(index + 1) % modes.length],
+    });
+  }, []);
   const className = [props.className, styles["switch"]].filter(Boolean).join(" ");
-  return (
-    <div {...props} className={className} data-testid="switch">
-      {children}
-    </div>
-  );
+  return <div {...props} className={className} data-testid="switch" onClick={handleModeSwitch} />;
 };
