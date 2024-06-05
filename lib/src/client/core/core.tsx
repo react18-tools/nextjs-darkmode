8eb3b616cb32aebe3f9deb51bc5d79ac3943411b
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-06-05 22:06:20 +0530

    hadle media:prefers-color-scheme updates

--- lib/src/client/core/core.tsx
+++ lib/src/client/core/core.tsx
@@ -1,4 +1,5 @@
-import useRGS from "r18gs";
+import { useStore } from "../../utils";
+import { useEffect } from "react";
 
 export interface CoreProps {
   /** force apply CSS transition property to all the elements during theme switching. E.g., `all .3s` */
@@ -16,5 +17,19 @@ export interface CoreProps {
  * @source - Source code
  */
 export const Core = ({ t }: CoreProps) => {
+  const [{ mode, systemMode }, setThemeState] = useStore();
+  const resolvedMode = mode === "system" ? systemMode : mode; // resolvedMode is the actual mode that will be used
+
+  useEffect(() => {
+    const media = matchMedia("(prefers-color-scheme: dark)");
+    const updateSystemColorScheme = () => {
+      setThemeState(state => ({ ...state, systemMode: media.matches ? "dark" : "light" }));
+    };
+    updateSystemColorScheme();
+    media.addEventListener("change", updateSystemColorScheme);
+    return () => {
+      media.removeEventListener("change", updateSystemColorScheme);
+    };
+  }, []);
   return null;
 };
