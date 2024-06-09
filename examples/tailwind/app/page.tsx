9605fe419fa7f8c802db40c10c73a6b99c4baaf9
Mayank <mayank.srmu@gmail.com>
2024-06-09 20:51:14 +0530

    fix: fix deepsource

--- examples/tailwind/app/page.tsx
+++ examples/tailwind/app/page.tsx
@@ -2,8 +2,10 @@ import { StarMe } from "@mayank1513/fork-me";
 import { Switch } from "nextjs-darkmode/switch";
 import Card from "./_components/card";
 
+// Home page
 export default function Home() {
   return (
+    // skipcq: JS-0415
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
