034339567596baebc92cc09df8904fd18f61f02d
Mayank <mayank.srmu@gmail.com>
2024-06-09 11:51:48 +0530

    Fix: update styles  in vite app

--- examples/vite/src/app/index.tsx
+++ examples/vite/src/app/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import { LandingPage, Layout } from "@repo/shared/dist/server";
+import { Header } from "@repo/shared";
 import { Core } from "nextjs-darkmode";
 
 /** Vite App */
@@ -8,6 +9,7 @@ function App(): JSX.Element {
   return (
     <Layout>
       <Core />
+      <Header />
       <LandingPage title="Vite Example" />
     </Layout>
   );
