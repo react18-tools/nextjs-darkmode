19514b004a71b335a97887f57ca6ea190de42767
Mayank <mayank.srmu@gmail.com>
2024-06-14 20:49:42 +0530

    Create tailwind-ssg example

---
+++ examples/tailwind-ssg/app/page.tsx
@@ -0,0 +1,61 @@
+import { StarMe } from "@mayank1513/fork-me";
+import { Switch } from "nextjs-darkmode/switch";
+import Card from "./_components/card";
+
+// Home page
+export default function Home() {
+  return (
+    // skipcq: JS-0415
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          nextjs-darkmode Tailwind CSS example
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://mayank-chaudhari.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer">
+            By Mayank
+          </a>
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]" />
+
+      <Switch />
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Card
+          href="https://react18-tools.github.io/nextjs-darkmode/"
+          title="Docs"
+          text="Find in-depth information about nextjs-darkmode features and API."
+        />
+        <Card
+          href="https://github.com/react18-tools/nextjs-darkmode"
+          title="Examples"
+          text="Learn through more examples on official GitHub repo."
+        />
+        <StarMe
+          gitHubUrl="https://github.com/react18-tools/nextjs-darkmode"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+          <h2 className={"mb-3 text-2xl font-semibold"}>
+            Star Me{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={"m-0 max-w-[30ch] text-sm opacity-50"}>
+            Explore and star official <code>nextjs-darkmode</code> repo.
+          </p>
+        </StarMe>
+        <Card
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          title="Deploy"
+          text="Instantly deploy your Next.js site to a shareable URL with Vercel."
+        />
+      </div>
+    </main>
+  );
+}
