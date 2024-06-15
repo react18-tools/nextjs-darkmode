faff3f07e73bc94f8133d08aa689633e478caa2a
Mayank <mayank.srmu@gmail.com>
2024-06-15 15:19:45 +0530

    clean up

--- lib/src/constants.ts
+++ lib/src/constants.ts
@@ -4,7 +4,4 @@ export const SYSTEM: ColorSchemePreference = "system";
 export const DARK: ColorSchemePreference = "dark";
 export const LIGHT: ColorSchemePreference = "";
 
-export const COOKIE_KEY = "gx";
 export const modes: ColorSchemePreference[] = [SYSTEM, DARK, LIGHT];
-
-export const MEDIA = `(prefers-color-scheme: ${DARK})`;
