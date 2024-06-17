744535ce6177b48a5719f0e0569d3eb80849e81e
Mayank <mayank.srmu@gmail.com>
2024-06-17 13:59:42 +0530

    fix style imports

--- examples/nextjs-pages/src/pages/index.tsx
+++ examples/nextjs-pages/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 import { Switch } from "nextjs-darkmode/switch";
 
 const inter = Inter({ subsets: ["latin"] });
