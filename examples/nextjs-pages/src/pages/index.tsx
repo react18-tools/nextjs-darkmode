75f9322fd1c8d8182b52597be104c39820b2fe54
Mayank <mayank.srmu@gmail.com>
2024-06-17 14:12:57 +0530

    pages example builds well when no rendering nextjs-darkmode components

--- examples/nextjs-pages/src/pages/index.tsx
+++ examples/nextjs-pages/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "../styles/Home.module.css";
 import { Switch } from "nextjs-darkmode/switch";
+import { Core } from "nextjs-darkmode";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Switch />
+        {/* <Core /> */}
+        {/* <Switch /> */}
       </main>
     </>
   );
